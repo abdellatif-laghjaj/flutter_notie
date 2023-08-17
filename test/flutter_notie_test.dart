beccfdd68054a6398f66f4b151fd4687dc29db4d
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-08-17 22:54:23 +0100

    Update flutter_notie_test.dart

--- test/flutter_notie_test.dart
+++ test/flutter_notie_test.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_notie/flutter_notie.dart';
 
 void main() {
   test('adds one to input values', () {});
